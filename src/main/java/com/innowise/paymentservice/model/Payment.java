868fa101185c47b95da96142b082820e38b7b687
setor <marik123456789110011@gmail.com>
2026-06-24 19:15:14 +0300

    feet: model layer

--- src/main/java/com/innowise/paymentservice/model/Payment.java
+++ src/main/java/com/innowise/paymentservice/model/Payment.java
@@ -1,6 +1,6 @@
 package com.innowise.paymentservice.model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.math.BigDecimal;
@@ -15,16 +15,25 @@ import java.time.LocalDateTime;
 @Setter
 public class Payment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
     private Long id;
 
+    @Column(name = "order_id",nullable = false)
     private Long order_id;
 
+    @Column(name = "user_id",nullable = false)
     private Long user_id;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status",nullable = false)
     private Status status;
 
+    @Column(name = "timestamp",nullable = false)
     private LocalDateTime timestamp;
 
-    private BigDecimal payment_amount;
+    @Column(name = "payment_amount",nullable = false)
+    private BigDecimal paymentAmount;
 
 }
